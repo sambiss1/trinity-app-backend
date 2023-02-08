4bebbe6e201dfbad4d419bc9baddb91ad4750029
Sam <sambisselele@gmail.com>
2023-02-08 18:57:14 +0100

    Feat : create extension schema

---
+++ src/schemas/extension.schema.ts
@@ -0,0 +1,27 @@
+
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+import * as mongoose from 'mongoose'
+import { Company } from './company.schema';
+
+export type ExtensionDocument = HydratedDocument<Extension>;
+
+@Schema()
+export class Extension {
+    @Prop({ required: true })
+    name: string;
+
+    @Prop()
+    location: string;
+
+    @Prop()
+    director: string;
+
+    @Prop()
+    slug: string;
+
+    @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'Company' })
+    company: Company;
+}
+
+export const ExtensionSchema = SchemaFactory.createForClass(Extension);

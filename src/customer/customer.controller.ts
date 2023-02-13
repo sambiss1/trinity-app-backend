8cadfd7f08a4a23d51f4ed5d1598ea338d786bd1
Sam <sambisselele@gmail.com>
2023-02-13 15:52:52 +0100

    Feat : create customer controllers

--- src/customer/customer.controller.ts
+++ src/customer/customer.controller.ts
@@ -7,28 +7,40 @@ import { UpdateCustomerDto } from './dto/update-customer.dto';
 export class CustomerController {
   constructor(private readonly customerService: CustomerService) {}
 
+  // Post
   @Post()
   create(@Body() createCustomerDto: CreateCustomerDto) {
     return this.customerService.create(createCustomerDto);
   }
 
+  // Get all
   @Get()
   findAll() {
     return this.customerService.findAll();
   }
 
+
+  // Get one
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.customerService.findOne(+id);
   }
 
+  // Update one
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateCustomerDto: UpdateCustomerDto) {
     return this.customerService.update(+id, updateCustomerDto);
   }
 
+  // Delete one
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.customerService.remove(+id);
+  }
+
+  // Delete many
+  @Delete()
+  deleteAll() {
+    return this.customerService.deleteAll();
   }
 }

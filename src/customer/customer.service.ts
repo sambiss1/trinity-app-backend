49c571571fbf619320f63cae3bb4d0d498f6e84e
Sam <sambisselele@gmail.com>
2023-02-13 15:43:15 +0100

    Feat : create customer services

--- src/customer/customer.service.ts
+++ src/customer/customer.service.ts
@@ -1,26 +1,48 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCustomerDto } from './dto/create-customer.dto';
 import { UpdateCustomerDto } from './dto/update-customer.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { SoftDeleteModel } from 'soft-delete-plugin-mongoose';
+import { Customer, CustomerDocument } from 'src/schemas/customer.schema';
 
 @Injectable()
 export class CustomerService {
-  create(createCustomerDto: CreateCustomerDto) {
-    return 'This action adds a new customer';
+
+  constructor(@InjectModel(Customer.name) private customerModel: SoftDeleteModel<CustomerDocument>) { }
+
+  // Create customer
+  async create(createCustomerDto: CreateCustomerDto): Promise<Customer> {
+    const createdCustomer = await new this.customerModel(createCustomerDto).save();
+    return createdCustomer
   }
 
-  findAll() {
-    return `This action returns all customer`;
+  // Find all customer
+  async findAll() {
+    return this.customerModel.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} customer`;
+  // Find one customer
+  async findOne(id: number) {
+    return await this.customerModel.findOne({ id }).populate("company");
+
   }
 
-  update(id: number, updateCustomerDto: UpdateCustomerDto) {
-    return `This action updates a #${id} customer`;
+  // Update one
+  async update(id: number, updateCustomerDto: UpdateCustomerDto) {
+    return await this.customerModel.updateOne({ id }, { $set: { ...updateCustomerDto } });
+
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} customer`;
+  // Remove one
+  async remove(id: number) {
+    const deleted = this.customerModel.softDelete({ _id: id });
+    return deleted;
+  }
+
+  // Delete many
+
+  // remove many 
+  async deleteAll() {
+    return await this.customerModel.deleteMany();
   }
 }

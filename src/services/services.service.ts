8498ce509395994c5b98d4075c00ed640abd4daa
Sam <sambisselele@gmail.com>
2023-02-13 16:56:16 +0100

    Feat :  create service

--- src/services/services.service.ts
+++ src/services/services.service.ts
@@ -1,26 +1,44 @@
 import { Injectable } from '@nestjs/common';
 import { CreateServiceDto } from './dto/create-service.dto';
 import { UpdateServiceDto } from './dto/update-service.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { SoftDeleteModel } from 'soft-delete-plugin-mongoose';
+import { Service, ServiceDocument } from 'src/schemas/services.schemas';
 
 @Injectable()
 export class ServicesService {
-  create(createServiceDto: CreateServiceDto) {
-    return 'This action adds a new service';
+
+  constructor(@InjectModel(Service.name) private serviceModel: SoftDeleteModel<ServiceDocument>) { }
+
+  // Create service
+  async create(createServiceDto: CreateServiceDto): Promise<Service> {
+    const createdService = await new this.serviceModel(createServiceDto).save();
+    return createdService
   }
 
-  findAll() {
-    return `This action returns all services`;
+  // Find all
+  async findAll() {
+    return await this.serviceModel.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} service`;
+  // Find one
+  async findOne(id: number) {
+    return await this.serviceModel.findOne({ id })
   }
 
-  update(id: number, updateServiceDto: UpdateServiceDto) {
-    return `This action updates a #${id} service`;
+  // Update one
+  async update(id: number, updateServiceDto: UpdateServiceDto) {
+    return await this.serviceModel.updateOne({ id }, { $set: { ...updateServiceDto } });
   }
 
+  // Remove one
   remove(id: number) {
-    return `This action removes a #${id} service`;
+    const deleted = this.serviceModel.softDelete({ _id: id });
+    return deleted;
+  }
+
+  // Remove many
+  async deleteAll() {
+    return await this.serviceModel.deleteMany();
   }
 }

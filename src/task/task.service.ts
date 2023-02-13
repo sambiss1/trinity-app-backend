07118dc74d8b24a2aed7e88c4040ba7202db509e
Sam <sambisselele@gmail.com>
2023-02-13 17:43:05 +0100

    Feat :  create task services

--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,26 +1,44 @@
 import { Injectable } from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { SoftDeleteModel } from 'soft-delete-plugin-mongoose';
+import { Task, TaskDocument } from 'src/schemas/task.schema';
 
 @Injectable()
 export class TaskService {
-  create(createTaskDto: CreateTaskDto) {
-    return 'This action adds a new task';
+  constructor(@InjectModel(Task.name) private taskModel: SoftDeleteModel<TaskDocument>) { }
+
+  // Create task
+  async create(createTaskDto: CreateTaskDto): Promise<Task> {
+    const createdTask = await new this.taskModel(createTaskDto).save();
+    return createdTask;
   }
 
-  findAll() {
-    return `This action returns all task`;
+  // find all tasks
+  async findAll() {
+    return await this.taskModel.find().populate([{ path: "service" }, { path: "taskers" }]);
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} task`;
+  // Find one
+  async findOne(id: number) {
+    return await this.taskModel.findOne({ id }).populate([{ path: "service" }, { path: "taskers" }]);
   }
 
-  update(id: number, updateTaskDto: UpdateTaskDto) {
-    return `This action updates a #${id} task`;
+  // Update one
+  async update(id: number, updateTaskDto: UpdateTaskDto) {
+    return this.taskModel.updateOne({ id }, { $set: { ...updateTaskDto } });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} task`;
+  // Delete one
+  async remove(id: number) {
+    const deleted = this.taskModel.softDelete({ _id: id });
+    return deleted;
+  }
+
+
+  // remove many 
+  async deleteAll() {
+    return await this.taskModel.deleteMany();
   }
 }

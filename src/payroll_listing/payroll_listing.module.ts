0aa532c7e74d37e06cd768c3ec9969695454a391
Sam <sambisselele@gmail.com>
2023-02-13 15:20:23 +0100

    Feat :  create payroll module

--- src/payroll_listing/payroll_listing.module.ts
+++ src/payroll_listing/payroll_listing.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PayrollListingService } from './payroll_listing.service';
 import { PayrollListingController } from './payroll_listing.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { PayrollListing, PayrollListingSchema } from 'src/schemas/payrollListing.schema';
+
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: PayrollListing.name, schema: PayrollListingSchema }])],
   controllers: [PayrollListingController],
   providers: [PayrollListingService]
 })

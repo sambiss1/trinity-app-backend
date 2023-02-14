a3908b32d8838142f25d1b7a767a4225ae87492f
Sam <sambisselele@gmail.com>
2023-02-14 08:37:01 +0100

    feat : create invoice module

--- src/invoice/invoice.module.ts
+++ src/invoice/invoice.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { InvoiceService } from './invoice.service';
 import { InvoiceController } from './invoice.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Invoice, InvoiceSchema } from 'src/schemas/invoice.schema';
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: Invoice.name, schema: InvoiceSchema }])],
   controllers: [InvoiceController],
   providers: [InvoiceService]
 })

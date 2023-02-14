7d51a0d52c6751c28650bb0cc0e7c27e9a8e9c12
Sam <sambisselele@gmail.com>
2023-02-14 09:01:27 +0100

    feat :  create invoice module

--- src/invoice/invoice.controller.ts
+++ src/invoice/invoice.controller.ts
@@ -7,28 +7,40 @@ import { UpdateInvoiceDto } from './dto/update-invoice.dto';
 export class InvoiceController {
   constructor(private readonly invoiceService: InvoiceService) {}
 
+  // post one
   @Post()
   create(@Body() createInvoiceDto: CreateInvoiceDto) {
     return this.invoiceService.create(createInvoiceDto);
   }
 
+  // get all 
   @Get()
   findAll() {
     return this.invoiceService.findAll();
   }
 
+  // Get one
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.invoiceService.findOne(+id);
   }
 
+  // Update one
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateInvoiceDto: UpdateInvoiceDto) {
     return this.invoiceService.update(+id, updateInvoiceDto);
   }
 
+  // Delete one
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.invoiceService.remove(+id);
+  }
+
+
+  // Delete all
+  @Delete()
+  deleteAll() {
+    return this.invoiceService.deleteAll();
   }
 }

13bb4ca66ffda9e9b62af304a11377f3df8a1781
Sam <sambisselele@gmail.com>
2023-02-14 08:34:38 +0100

    Feat :  create invoice schema

---
+++ src/schemas/invoice.schema.ts
@@ -0,0 +1,41 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import * as mongoose from 'mongoose';
+import { HydratedDocument } from 'mongoose';
+import { softDeletePlugin } from 'soft-delete-plugin-mongoose';
+import { Customer } from './customer.schema';
+
+export type InvoiceDocument = HydratedDocument<Invoice>;
+
+@Schema({
+    timestamps: true,
+})
+export class Invoice {
+    @Prop({ required: true })
+    num: string;
+
+    @Prop()
+    period: string;
+
+    @Prop()
+    currency: string;
+
+    @Prop()
+    payed: boolean;
+
+    @Prop()
+    amount_due: string;
+
+    @Prop()
+    amount_paid: string;
+
+    @Prop()
+    payment_term: string;
+
+    @Prop()
+    date: string;
+
+    @Prop([{ type: mongoose.Schema.Types.ObjectId, ref: "Customer" }])
+    customer: Customer
+}
+
+export const InvoiceSchema = SchemaFactory.createForClass(Invoice).plugin(softDeletePlugin);

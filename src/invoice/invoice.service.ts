c56f9248f261fa3ac5c5db7e5417a51f4f7760a2
Sam <sambisselele@gmail.com>
2023-02-14 08:59:37 +0100

    Feat : create invoice service

--- src/invoice/invoice.service.ts
+++ src/invoice/invoice.service.ts
@@ -1,26 +1,45 @@
 import { Injectable } from '@nestjs/common';
 import { CreateInvoiceDto } from './dto/create-invoice.dto';
 import { UpdateInvoiceDto } from './dto/update-invoice.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { SoftDeleteModel } from 'soft-delete-plugin-mongoose';
+import { Invoice, InvoiceDocument } from 'src/schemas/invoice.schema';
 
 @Injectable()
 export class InvoiceService {
-  create(createInvoiceDto: CreateInvoiceDto) {
-    return 'This action adds a new invoice';
+
+
+  constructor(@InjectModel(Invoice.name) private invoiceModel: SoftDeleteModel<InvoiceDocument>) { }
+
+  // Create invoice
+  async create(createInvoiceDto: CreateInvoiceDto): Promise<Invoice> {
+    const createdInvoice = await new this.invoiceModel(createInvoiceDto).save();
+    return createdInvoice;
   }
 
-  findAll() {
-    return `This action returns all invoice`;
+  // Find all
+  async findAll() {
+    return this.invoiceModel.find().populate("customer");
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} invoice`;
+  // Find one
+  async findOne(id: number) {
+    return await this.invoiceModel.findOne({ id }).populate("customer");
   }
 
-  update(id: number, updateInvoiceDto: UpdateInvoiceDto) {
-    return `This action updates a #${id} invoice`;
+  // Update one
+  async update(id: number, updateInvoiceDto: UpdateInvoiceDto) {
+    return await this.invoiceModel.updateOne({ id }, { $set: { ...updateInvoiceDto } });
   }
 
+  // Remove one
   remove(id: number) {
-    return `This action removes a #${id} invoice`;
+    const deleted = this.invoiceModel.softDelete({ _id: id });
+    return deleted;
+  }
+
+  // Remove many
+  async deleteAll() {
+    return await this.invoiceModel.deleteMany();
   }
 }

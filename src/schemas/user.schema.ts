ad8da6207357f57710bcd5212144441d50348729
Sam <sambisselele@gmail.com>
2023-02-15 12:49:08 +0100

    Feat : create user schema

---
+++ src/schemas/user.schema.ts
@@ -0,0 +1,29 @@
+
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import * as mongoose from 'mongoose';
+import { HydratedDocument } from 'mongoose';
+import { Extension } from './extension.schema';
+
+export type UserDocument = HydratedDocument<User>;
+
+@Schema({
+    timestamps: true,
+})
+export class User {
+    @Prop({ required: true })
+    name: string;
+
+    @Prop({required: true})
+    email: string;
+
+    @Prop({required: true})
+    password: string;
+
+    @Prop({required: true})
+    rule: string;
+
+    @Prop({ type: mongoose.Schema.Types.ObjectId, ref: "Extension" }, { required: true })
+    extension: Extension
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);

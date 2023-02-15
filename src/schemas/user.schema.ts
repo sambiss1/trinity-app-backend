091c0468ca87aebc0e77ea04ba3847adb70cc762
Sam <sambisselele@gmail.com>
2023-02-15 14:18:03 +0100

    Feat :  create user service

--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -13,16 +13,16 @@ export class User {
     @Prop({ required: true })
     name: string;
 
-    @Prop({required: true})
+    @Prop({ required: true })
     email: string;
 
-    @Prop({required: true})
+    @Prop({ required: true })
     password: string;
 
-    @Prop({required: true})
+    @Prop({ required: true })
     rule: string;
 
-    @Prop({ type: mongoose.Schema.Types.ObjectId, ref: "Extension" }, { required: true })
+    @Prop({ type: mongoose.Schema.Types.ObjectId, ref: "Extension", required: true })
     extension: Extension
 }
 
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,26 +1,32 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { SoftDeleteModel } from 'soft-delete-plugin-mongoose';
+import { User, UserDocument } from 'src/schemas/user.schema';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(@InjectModel(User.name) private userModel: SoftDeleteModel<UserDocument>) { }
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    return (await this.userModel.create(createUserDto)).save();
   }
 
   findAll() {
-    return `This action returns all user`;
+    return this.userModel.find().populate("extension")
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne(query: object): Promise<User> {
+    return this.userModel.findOne(query);
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+    return this.userModel.updateOne({ id }, { $set: { ...updateUserDto } });
   }
 
   remove(id: number) {
-    return `This action removes a #${id} user`;
+    const deleted = this.userModel.softDelete({ _id: id });
+    return deleted;
   }
 }

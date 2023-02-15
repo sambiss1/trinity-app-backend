eb92fa3aa0035f63387696aa4a3fbec197dca2be
Sam <sambisselele@gmail.com>
2023-02-15 12:51:50 +0100

    Feat :  create user module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from "@nestjs/mongoose"
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
+import { User, UserSchema } from 'src/schemas/user.schema';
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])],
   controllers: [UserController],
   providers: [UserService]
 })
